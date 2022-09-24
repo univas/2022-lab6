402e69ebf9ee7501ba9f4cf968243de7b71206e6
Rodrigo Faria <rodrigolfsi@gmail.com>
2022-09-23 22:02:55 -0300

    Add swagger-ui-express

--- prova-01/index.js
+++ prova-01/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const swaggerUi = require('swagger-ui-express')
 const router = require('./router/movie.router')
 const auth = require('./middleware/auth')
+const swaggerJson = require('./openapi.json')
 const app = express()
 
 app.use(express.json())
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerJson))
 app.use(auth.authentication)
 app.use('/api/movies', router)
 
